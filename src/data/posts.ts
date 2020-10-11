01ea1947d17c6fe9c935432f3b5a3b994da4dd08
KushibikiMashu <kushibiki3413@gmail.com>
2020-10-11 21:56:29 +0900

    postのデータを追加

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -3,14 +3,20 @@ import { Posts } from '~/src/types'
 export const posts: Posts = [
   {
     id: 1,
+    title: 'Javascript',
+    body: 'JavaScript is the Programming Language for the Web.',
+    date: '2020-01-01',
+  },
+  {
+    id: 2,
     title: 'React',
     body: 'React is the JavaScript library for building UIs.',
-    date: '2020-01-01',
+    date: '2020-02-01',
   },
   {
     id: 2,
     title: 'Next.js',
     body: 'Next.js is the framework for React developers',
-    date: '2020-02-01',
+    date: '2020-03-01',
   },
 ]
