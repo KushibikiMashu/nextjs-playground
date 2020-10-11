26b069ba338833e9c1303133b66c5ebabe5442f9
KushibikiMashu <kushibiki3413@gmail.com>
2020-10-11 18:12:44 +0900

    IndexPageのcomponentとcontainerを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,39 @@
 import Link from 'next/link'
 import Greeting from 'src/components/Greeting'
 
-export default function IndexPage() {
-  return (
-    <div>
-      <div className="py-20">
-        <Greeting target={'Next.js Playground'} />
-      </div>
+type ContainerProps = undefined
 
-      <div className="py-10">
-        <h2 className="text-center text-2xl font-bold">Pages</h2>
-        <ul className="text-xl text-center">
-          <li className="m-6">
-            <Link href="/modals">
-              <a className="text-blue-600 visited:text-purple-600">modals</a>
+type Props = {
+  pages: string[]
+}
+
+const Component: React.FC<Props> = (props) => (
+  <div>
+    <div className="py-20">
+      <Greeting target={'Next.js Playground'} />
+    </div>
+
+    <div className="py-10">
+      <h2 className="text-center text-2xl font-bold">Pages</h2>
+      <ul className="text-xl text-center">
+        {props.pages.map((page) => (
+          <li className="m-6" key={page}>
+            <Link href={`/${page}`}>
+              <a className="text-blue-600 visited:text-purple-600">{page}</a>
             </Link>
           </li>
-        </ul>
-      </div>
+        ))}
+      </ul>
     </div>
-  )
+  </div>
+)
+
+const Container: React.FC<ContainerProps> = () => {
+  const pages = ['modals']
+
+  return <Component pages={pages} />
 }
+
+Container.displayName = 'IndexPage'
+
+export default Container
