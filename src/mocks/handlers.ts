0a04f580e6fa704aa2fa6b4236871e588926b6df
KushibikiMashu <kushibiki3413@gmail.com>
2020-10-13 00:24:57 +0900

    mswの設定を追加

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,8 +2,8 @@ import { rest } from 'msw'
 import { posts } from '~/src/data'
 
 export const handlers = [
-  rest.get('/api/post/1', (req, res, ctx) => {
+  rest.get('https://example.com/api/post/1', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(posts[0]))
   }),
-  rest.get('/api/posts', (req, res, ctx) => res(ctx.status(200), ctx.json(posts))),
+  rest.get('https://example.com/api/posts', (req, res, ctx) => res(ctx.status(200), ctx.json(posts))),
 ]
