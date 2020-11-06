36f8f08bf6e3eb8fcf490696ca9ad789a88ff883
KushibikiMashu <kushibiki3413@gmail.com>
2020-11-06 19:57:58 +0900

    feat: useImmerReducerに置き換え

--- src/hooks/useUserState.ts
+++ src/hooks/useUserState.ts
@@ -1,7 +1,5 @@
-import { useCallback, useReducer } from 'react'
-
-// TODO
-// タイムトラベルの機能を追加
+import { useCallback } from 'react'
+import { useImmerReducer } from 'use-immer'
 
 type State = {
   user: { loggedIn: boolean; name: string | null }
@@ -28,41 +26,82 @@ const initialState: State = {
   logs: [],
 }
 
-// TODO immerを試す
-const reducer = (state: State, action: Action): State => {
-  // Need some middleware lol
-  const logs = [...state.logs, action.type]
+// Usual reducer
+// const reducer = (state: State, action: Action): State => {
+//   // Need some middleware lol
+//   const logs = [...state.logs, action.type]
+//
+//   switch (action.type) {
+//     case 'USER::LOGIN':
+//       return { ...state, user: { ...state.user, loggedIn: true }, logs }
+//     case 'USER::LOGOUT':
+//       return { ...state, user: { name: null, loggedIn: false }, logs }
+//     case 'USER::CHANGE':
+//       return { ...state, user: { ...state.user, name: action.payload.name }, logs }
+//     case 'TODO::ADD':
+//       return { ...state, todos: [...state.todos, { ...action.payload }], logs }
+//     case 'TODO::DELETE':
+//       // eslint-disable-next-line no-case-declarations
+//       const todos = state.todos.filter((todo) => todo.id !== action.payload.id)
+//       return { ...state, todos, logs }
+//     case 'FETCH::LOADING':
+//       return { ...state, status: 'loading', logs }
+//     case 'FETCH::FULFILLED':
+//       return { ...state, status: 'fulfilled', logs }
+//     case 'FETCH::REJECTED':
+//       return { ...state, status: 'rejected', logs }
+//     case 'FETCH::RESET':
+//       return { ...state, status: null, logs }
+//     default:
+//       return state
+//   }
+// }
 
-  switch (action.type) {
+// reducer for immer
+const immerReducer = (draft: State, action: Action) => {
+  const type = action.type
+  draft.logs.push(type)
+
+  switch (type) {
     case 'USER::LOGIN':
-      return { ...state, user: { ...state.user, loggedIn: true }, logs }
+      draft.user.loggedIn = true
+      return
     case 'USER::LOGOUT':
-      return { ...state, user: { name: null, loggedIn: false }, logs }
+      draft.user.name = null
+      draft.user.loggedIn = false
+      return
     case 'USER::CHANGE':
-      return { ...state, user: { ...state.user, name: action.payload.name }, logs }
+      draft.user.name = action.payload.name
+      return
     case 'TODO::ADD':
-      return { ...state, todos: [...state.todos, { id: action.payload.id, title: action.payload.title }], logs }
+      draft.todos.push(action.payload)
+      return
     case 'TODO::DELETE':
       // eslint-disable-next-line no-case-declarations
-      const todos = state.todos.filter((todo) => todo.id !== action.payload.id)
-      return { ...state, todos, logs }
+      delete draft.todos[action.payload.id]
+      return
     case 'FETCH::LOADING':
-      return { ...state, status: 'loading', logs }
+      draft.status = 'loading'
+      return
     case 'FETCH::FULFILLED':
-      return { ...state, status: 'fulfilled', logs }
+      draft.status = 'fulfilled'
+      return
     case 'FETCH::REJECTED':
-      return { ...state, status: 'rejected', logs }
+      draft.status = 'rejected'
+      return
     case 'FETCH::RESET':
-      return { ...state, status: null, logs }
-    default:
-      return state
+      draft.status = null
   }
 }
 
 export const isChange = (type): type is 'USER::CHANGE' => type === 'USER::CHANGE'
 
 export const useUserState = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  // Usual
+  // const [state, dispatch] = useReducer(reducer, initialState)
+
+  // Immer
+  const [state, dispatch] = useImmerReducer(immerReducer, initialState)
 
   const userHandler = useCallback((type: 'USER::LOGIN' | 'USER::LOGOUT' | 'USER::CHANGE', name?: string) => {
     if (isChange(type)) {
