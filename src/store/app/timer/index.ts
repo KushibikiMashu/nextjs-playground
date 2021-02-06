4436de1772da1b0e8c426a03fe02807ed2e52aa2
KushibikiMashu <kushibiki3413@gmail.com>
2021-02-06 19:16:42 +0900

    feat: timer reducer を追加

---
+++ src/store/app/timer/index.ts
@@ -0,0 +1,34 @@
+// types
+const TICK = 'APP::TIMER_TICK'
+
+// actions
+export const serverRenderClick = () => (dispatch) =>
+  dispatch({
+    type: TICK,
+    payload: { light: false, ts: Date.now() },
+  })
+export const startClock = () => (dispatch) =>
+  setInterval(() => {
+    dispatch({
+      type: TICK,
+      payload: { light: true, ts: Date.now() },
+    })
+  }, 1000)
+
+// reducer
+const initialState = {
+  lastUpdate: 0,
+  light: false,
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TICK:
+      return {
+        lastUpdate: action.payload.ts,
+        light: !!action.payload.light,
+      }
+    default:
+      return state
+  }
+}
