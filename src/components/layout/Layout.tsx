2c353a0b8597bf568c07f6394cb0d9c8b9a77a6f
KushibikiMashu <kushibiki3413@gmail.com>
2021-02-06 18:01:44 +0900

    fix: トップページへのリンクのlocaleを変更

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -14,7 +14,7 @@ export const Component: React.FC<Props> = (props) => (
   <div className="flex flex-col min-h-screen">
     <nav className="w-screen flex items-center justify-between bg-white border-b border-gray-200 fixed inset-x-0 z-100 h-16">
       <p className="pl-8 text-teal-600 text-xl">
-        <CustomLink path={Paths.top}>
+        <CustomLink path={Paths.top} locale="en-US">
           <a>Next.js Playground</a>
         </CustomLink>
       </p>
@@ -31,7 +31,7 @@ export const Component: React.FC<Props> = (props) => (
       {props.children}
       {!props.isTop && (
         <div className="m-10 text-center">
-          <CustomLink path={Paths.top}>
+          <CustomLink path={Paths.top} locale="en-US">
             <a className="text-blue-600 visited:text-purple-600">Top</a>
           </CustomLink>
         </div>
