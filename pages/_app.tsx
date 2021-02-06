9cf31077ef49a64503dafdbf586dfa00bff6b7cb
KushibikiMashu <kushibiki3413@gmail.com>
2021-02-06 19:36:03 +0900

    feat: redux を Next.js に接続

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import './style.scss'
+import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { Provider } from 'react-redux'
 import Layout from '~/src/components/layout'
+import { useStore } from '~/src/store/store'
 
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps) => {
+  const store = useStore(pageProps.initialReduxState)
   const router = useRouter()
   const pathname = router.pathname
 
   return (
-    <Layout pathname={pathname}>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout pathname={pathname}>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
