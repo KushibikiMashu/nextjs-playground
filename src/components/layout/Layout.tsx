e6ac85dae9e0f2e2bb522ad62b7d2f99b97382ea
KushibikiMashu <kushibiki3413@gmail.com>
2021-02-06 23:01:28 +0900

    feat: 30秒ごとにタイマーの色を変える

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,9 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import CustomLink from '~/src/components/_shared/CustomLink'
 import { GITHUB_REPOSITORY_URL, Paths } from '~/src/constants'
+
+import { startClock } from '~/src/store/app/timer'
 
 type ContainerProps = { pathname: string }
 
@@ -8,9 +11,12 @@ type Props = {
   children?: React.ReactNode
   isTop: boolean
   gitHubUrl: string
+  timerFlag: boolean
+  time: string
 }
 
-export const Component: React.FC<Props> = (props) => (
+// eslint-disable-next-line react/display-name
+export const Component: React.FC<Props> = memo((props) => (
   <div className="flex flex-col min-h-screen">
     <nav className="w-screen flex items-center justify-between bg-white border-b border-gray-200 fixed inset-x-0 z-100 h-16">
       <p className="pl-8 text-teal-600 text-xl">
@@ -36,6 +42,18 @@ export const Component: React.FC<Props> = (props) => (
           </CustomLink>
         </div>
       )}
+      {/* timer */}
+      <div
+        className={`fixed bg-black px-4 py-2 rounded-t-md`}
+        style={{
+          bottom: 0,
+          right: 24,
+          fontFamily: 'menlo, monaco, monospace',
+          color: props.timerFlag ? '#FFFFFF' : '#86EFAC',
+        }}
+      >
+        <time dateTime={props.time}>{props.time}</time>
+      </div>
     </div>
     <footer className="py-8 flex justify-center items-center border-t border-gray-200">
       Created By{' '}
@@ -44,15 +62,36 @@ export const Component: React.FC<Props> = (props) => (
       </a>
     </footer>
   </div>
-)
+))
 
 const Container: React.FC<ContainerProps> = memo(
   (props) => {
+    const { tickCount } = useSelector((state) => state.ui)
+    const { lastUpdate } = useSelector((state) => state.app.timer)
+    const dispatch = useDispatch()
+    // timer の色を変える
+    const [timerFlag, setTimerFlag] = useState(false)
+
+    useEffect(() => {
+      dispatch(startClock())
+    }, [])
+
+    // tick の回数 30 回ごとにフラグを toggle する
+    useEffect(() => {
+      const canDivideByThirty = tickCount !== 0 && tickCount % 30 === 0
+      if (canDivideByThirty) {
+        setTimerFlag((state) => !state)
+      }
+    }, [tickCount])
+
+    // link
     const isTop = props.pathname === '/'
     const githubPath = isTop ? '' : `/blob/main/pages${props.pathname}.tsx`
     const gitHubUrl = GITHUB_REPOSITORY_URL + githubPath
 
-    return <Component {...props} isTop={isTop} gitHubUrl={gitHubUrl} />
+    const time = new Date(lastUpdate).toLocaleString('ja-JP').slice(9)
+
+    return <Component {...props} timerFlag={timerFlag} isTop={isTop} gitHubUrl={gitHubUrl} time={time} />
   },
   (prev, next) => prev.children === next.children
 )
