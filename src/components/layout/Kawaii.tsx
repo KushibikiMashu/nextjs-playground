fe719f8f315a9e0e6b3c37be3473bdc190c19a9d
KushibikiMashu <kushibiki3413@gmail.com>
2021-02-12 14:58:03 +0900

    refactor: Kawaii の状態をシンプルにする

--- src/components/layout/Kawaii.tsx
+++ src/components/layout/Kawaii.tsx
@@ -1,5 +1,8 @@
-import React, { MutableRefObject, PropsWithChildren, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Backpack, Ghost, IceCream } from 'react-kawaii'
+
+// 本当は setTimeout で 30秒ごとに表示/非表示の制御をするのが一番簡単に実現できる
+// ただ、今回は Redux の store の値の変化をトリガーにするため、下記のような実装にしている
 
 type Ref = {
   isFirstTime: boolean
@@ -8,19 +11,17 @@ type Ref = {
 
 type ContainerProps = { fired: boolean }
 
-type Props = PropsWithChildren<{
-  canDisplay: boolean
-  customRef: MutableRefObject<Ref>
-}>
+type Props = { display: 'block' | 'none' }
 
 // eslint-disable-next-line react/display-name
-const Component = (props: Props) => (
+const Component: React.FC<Props> = (props) => (
   <div
     className="kawaii"
     style={{
       // 初回アクセス時は表示しない
-      display: !props.customRef.current.isFirstTime && props.canDisplay ? 'block' : 'none',
+      display: props.display,
     }}
+    data-qa="kawaii"
   >
     <a href="https://react-kawaii.now.sh/" target="_blank" rel="noopener noreferrer">
       {props.children}
@@ -33,7 +34,7 @@ const Container: React.FC<ContainerProps> = (props) => {
     isFirstTime: true,
     index: 0,
   })
-  const [canDisplay, display] = useState(false)
+  const [canDisplay, setDisplay] = useState(false)
 
   const common = { size: 160 }
   const components = [
@@ -43,26 +44,27 @@ const Container: React.FC<ContainerProps> = (props) => {
   ]
 
   useEffect(() => {
-    display(true)
+    setDisplay(true)
   }, [props.fired])
 
   useEffect(() => {
     setTimeout(() => {
-      display(false)
+      setDisplay(false)
       // shuffle
       const index = Math.floor(Math.random() * Math.floor(components.length))
       ref.current = {
         isFirstTime: false,
         index,
       }
+      // 5000ms ではうまくいかないので、4900ms にしている。
     }, 4900)
   }, [props.fired])
 
-  return (
-    <Component canDisplay={canDisplay} customRef={ref}>
-      {components[ref.current.index]}
-    </Component>
-  )
+  // 初回アクセス時は表示しない
+  // 以降は、変数 canDisplay で判定する
+  const display = ref.current.isFirstTime ? 'none' : canDisplay ? 'block' : 'none'
+
+  return <Component display={display}>{components[ref.current.index]}</Component>
 }
 
 export default Container
