42298d6b14613f41761673ecab976f69b9cd69c5
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-24 22:26:52 +0900

    feat: SessionProvider を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '~/src/assets/style.scss'
+import { Provider as SessionProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { Provider } from 'react-redux'
+import { Provider as StoreProvider } from 'react-redux'
 import Layout from '~/src/components/layout'
 import { useStore } from '~/src/store/store'
 
@@ -11,11 +12,13 @@ const App = ({ Component, pageProps }: AppProps) => {
   const pathname = router.pathname
 
   return (
-    <Provider store={store}>
-      <Layout pathname={pathname}>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <StoreProvider store={store}>
+      <SessionProvider session={pageProps.session}>
+        <Layout pathname={pathname}>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </StoreProvider>
   )
 }
 
