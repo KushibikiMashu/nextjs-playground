c7df81b7b5323124586ef323ae850daf243bf18e
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-24 23:14:16 +0900

    feat: protected page を追加

--- pages/next-auth/protected.tsx
+++ pages/next-auth/protected.tsx
@@ -1,14 +1,30 @@
+import { signIn, useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
 
 const NextAuthProtectedPage = () => {
+  const [session] = useSession()
   const router = useRouter()
   const handleClick = () => router.back()
 
   return (
     <div className="mx-8">
-      <button type="button" onClick={handleClick}>
-        <span className="link">back</span>
-      </button>
+      <div>
+        {session ? (
+          'Now you are logged in!'
+        ) : (
+          <p>
+            You have to{' '}
+            <button className="link" type="button" onClick={() => signIn()}>
+              Sign in
+            </button>
+          </p>
+        )}
+      </div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          <span className="link">back</span>
+        </button>
+      </div>
     </div>
   )
 }
