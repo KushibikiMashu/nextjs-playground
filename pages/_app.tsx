28b55ee8af25b49c8cf3af82998b2bb4b70efd94
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-26 00:57:34 +0900

    feat: Apollo Provider を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '~/src/assets/style.scss'
+import { ApolloProvider } from '@apollo/client'
 import { Provider as SessionProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { Provider as StoreProvider } from 'react-redux'
 import Layout from '~/src/components/layout'
+import client from '~/src/lib/apollo-client'
 import { useStore } from '~/src/store/store'
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -12,13 +14,15 @@ const App = ({ Component, pageProps }: AppProps) => {
   const pathname = router.pathname
 
   return (
-    <StoreProvider store={store}>
-      <SessionProvider session={pageProps.session}>
-        <Layout pathname={pathname}>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
-    </StoreProvider>
+    <ApolloProvider client={client}>
+      <StoreProvider store={store}>
+        <SessionProvider session={pageProps.session}>
+          <Layout pathname={pathname}>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </StoreProvider>
+    </ApolloProvider>
   )
 }
 
