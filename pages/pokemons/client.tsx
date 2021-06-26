68877aacb7c2e54045e0207cdada0223b9477055
KushibikiMashu <kushibiki3413@gmail.com>
2021-06-26 23:20:29 +0900

    feat: 入力欄追加

--- pages/pokemons/client.tsx
+++ pages/pokemons/client.tsx
@@ -1,19 +1,41 @@
 import { gql, useQuery } from '@apollo/client'
+import { useState } from 'react'
 import CustomLink from '~/src/components/_shared/CustomLink/CustomLink'
 import PokemonList from '~/src/components/pokemon'
 import { Paths } from '~/src/constants'
 import { FindPokemonsQuery } from '~/src/graphql/pokemon/generated/client'
 
 const Query = gql`
-  query FindPokemons {
-    pokemons(first: 9) {
+  query FindPokemons($first: Int!) {
+    pokemons(first: $first) {
       name
     }
   }
 `
 
 const PokemonClient: React.VFC = () => {
-  const { data, loading, error } = useQuery<FindPokemonsQuery>(Query)
+  const [first, setFirst] = useState(9)
+  const { data, loading, error } = useQuery<FindPokemonsQuery>(Query, {
+    variables: { first },
+  })
+  const [numberError, setNumberError] = useState('')
+  const handleChange = (e) => {
+    const value = e.target.value
+
+    if (value === '') {
+      setFirst(0)
+      return
+    }
+
+    if (value < 0 || value > 151) {
+      const message = `Invalid value: ${value}. Number must be between 1 and 151.`
+      setNumberError(message)
+      return
+    }
+
+    setNumberError('')
+    setFirst(e.target.value)
+  }
 
   if (loading) {
     return <p className="pt-8 text-center">loading...</p>
@@ -28,6 +50,25 @@ const PokemonClient: React.VFC = () => {
       <h1 className="pt-8 pb-4 text-xl text-center">
         <span className="font-bold">Client</span> Side Rendering
       </h1>
+
+      <div className="pt-4 pb-8 text-center space-x-4 flex flex-col justify-center">
+        <div className="pb-4">
+          <input
+            id="count"
+            className="w-32 px-2 py-1 border border-teal-600 rounded-md focus:outline-none"
+            type="number"
+            min="1"
+            max="151"
+            placeholder="9"
+            onChange={handleChange}
+            value={first}
+          />
+          {numberError && <p className="pt-2 text-red-600">{numberError}</p>}
+        </div>
+        <label className="text-sm" htmlFor="count">
+          The number of Pokemons you want to fetch
+        </label>
+      </div>
 
       <PokemonList pokemons={data.pokemons} />
 
--- src/components/pokemon/PokemonList.tsx
+++ src/components/pokemon/PokemonList.tsx
@@ -4,14 +4,17 @@ type Props = FindPokemonsQuery
 
 const PokemonList: React.VFC<Props> = (props) => {
   return (
-    <div className="flex justify-center">
-      <ul className="pb-16">
-        {props.pokemons.map((pokemon, i) => (
-          <li className="py-2" key={pokemon.name}>
-            <span>00{i + 1}</span>: {pokemon.name}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h2 className="pb-4 text-xl text-teal-600 text-center">Pokemon List</h2>
+      <div className="flex justify-center">
+        <ul className="pb-16">
+          {props.pokemons.map((pokemon, i) => (
+            <li className="py-2" key={pokemon.name}>
+              <span>{`${i + 1}`.padStart(3, '00')}</span>: {pokemon.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
